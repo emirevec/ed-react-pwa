9f0285578d0a07547514b44fbb9d19db22b52e90
emirevec <vecchioemiliano@gmail.com>
2024-01-26 16:45:25 -0300

    build: set 'content' to tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./app/components/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./public/**/*.html",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/flowbite/**/*.js",
+    "./node_modules/flowbite-react/**/*.js"
+  ],
   theme: {
     extend: {},
   },
