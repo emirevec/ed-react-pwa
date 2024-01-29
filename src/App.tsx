e637a4d19e629da943d6920073fc818cbb7283d2
emirevec <vecchioemiliano@gmail.com>
2024-01-29 08:26:08 -0300

    build: update 'content' to tailwind.config.js file

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      
+      <h1 className='text-red-500'>Virtual Store Plus</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,jsx,ts,tsx}",
-    "./app/components/**/*.{js,jsx,ts,tsx}",
-    "./pages/**/*.{js,jsx,ts,tsx}",
-    "./public/**/*.html",
-    "./components/**/*.{js,jsx,ts,tsx}",
+    "./public/*.html",
+    "./scr/**/*.{js,jsx,ts,tsx}",
     "./node_modules/flowbite/**/*.js",
     "./node_modules/flowbite-react/**/*.js"
   ],
