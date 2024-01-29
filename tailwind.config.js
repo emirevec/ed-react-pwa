0e9fdf4c82810903fae38603aa3c4f541229a2d0
emirevec <vecchioemiliano@gmail.com>
2024-01-29 08:53:42 -0300

    build: add postcss and autoprefixer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./public/*.html",
-    "./scr/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/flowbite/**/*.js",
-    "./node_modules/flowbite-react/**/*.js"
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
     extend: {},
