e9061f0d4e02c3b5b0f210d791f07a13ca401b82
emirevec <vecchioemiliano@gmail.com>
2024-01-30 08:57:23 -0300

    feat: add ButtonHamburguer to Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import ButtonHamburguer from '../common/ButtonHamburguer'
+
 
 const Navbar = () => {
     return (
@@ -8,12 +10,7 @@ const Navbar = () => {
                     <img src="./src/images/icons/icon-144x144.png" className="h-8" alt="Virtual Store Logo" />
                     <span className="self-center text-2xl text-normal font-semibold whitespace-nowrap dark:text-white">Virtual Sotre Plus</span>
                 </a>
-                <button data-collapse-toggle="navbar-hamburger" type="button" className="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-normal rounded-lg border-2 border-green-500 hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-green-200 active:outline-none active:ring-2 active:ring-green-500 active:ring-offset-2 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-hamburger" aria-expanded="false">
-                    <span className="sr-only">Open main menu</span>
-                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-                    </svg>
-                </button>
+                <ButtonHamburguer></ButtonHamburguer>
                 <div className=" w-full border-t-2 border-normal mt-4" id="navbar-hamburger">
                     <ul className="flex flex-col font-medium rounded-lg pt-2 dark:bg-gray-800 dark:border-gray-700">
                         <li>
