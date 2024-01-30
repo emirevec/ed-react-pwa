6241dbf88c8fc9b4a89e9e77ac54f85332175d23
emirevec <vecchioemiliano@gmail.com>
2024-01-30 13:04:21 -0300

    feat: add NavbarMenu to Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import ButtonHamburguer from '../common/ButtonHamburguer'
+import NavbarMenu from './NavbarMenu'
 
 
 const Navbar = () => {
+    const items = ["Op 1", "Op 2", "Op 3"]
+
     return (
         <nav className="border-green-600 bg-theme dark:bg-green-900 dark:border-green-700">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -11,19 +14,8 @@ const Navbar = () => {
                     <span className="self-center text-2xl text-normal font-semibold whitespace-nowrap dark:text-white">Virtual Sotre Plus</span>
                 </a>
                 <ButtonHamburguer></ButtonHamburguer>
-                <div className=" w-full border-t-2 border-normal mt-4" id="navbar-hamburger">
-                    <ul className="flex flex-col font-medium rounded-lg pt-2 dark:bg-gray-800 dark:border-gray-700">
-                        <li>
-                            <a href="#" className="block py-2 px-3 text-normal hover:bg-green-500 hover:text-green-50 rounded-md px-3 py-2 font-medium  focus:outline-none focus:ring-2 focus:ring-green-200 active:outline-none active:ring-2 active:ring-green-500 active:ring-offset-2" aria-current="page">Store</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block py-2 px-3 text-normal hover:bg-green-500 hover:text-green-50 rounded-md px-3 py-2  font-medium focus:outline-none focus:ring-2 focus:ring-green-200 active:outline-none active:ring-2 active:ring-green-500 active:ring-offset-2">Categories</a>
-                        </li>
-                        <li>
-                            <a href="#" className="block py-2 px-3 text-normal hover:bg-green-500 hover:text-green-50 rounded-md px-3 py-2  font-medium focus:outline-none focus:ring-2 focus:ring-green-200 active:outline-none active:ring-2 active:ring-green-500 active:ring-offset-2">Watchlist</a>
-                        </li>
-                    </ul>
-                </div>
+                <NavbarMenu items={items} ></NavbarMenu>
+                
             </div>
         </nav>
     )
