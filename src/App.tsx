b28198878d705a204fc26abee739ad2c2193964b
emirevec <vecchioemiliano@gmail.com>
2024-02-01 17:32:43 -0300

    feat: add Gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Gallery from './components/Gallery/Gallery'
 
 function App (): JSX.Element {
+  const items = ['Jeans', 'Sneakers']
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Gallery items={items}></Gallery>
     </div>
   )
 }
