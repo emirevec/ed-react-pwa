8dca5c26da58cec7f54c1f8aa798eda226d06be0
emirevec <vecchioemiliano@gmail.com>
2024-02-05 09:37:52 -0300

    feat: remove cart from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Gallery from './components/Gallery/Gallery'
-import Cart from './components/Cart/Cart'
 
 function App (): JSX.Element {
   return (
     <div className="App">
       <Navbar></Navbar>
       <Gallery></Gallery>
-      <Cart></Cart>
     </div>
   )
 }
