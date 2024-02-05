c7c7d5154133f64f93a3ed980bcd21841b2e7234
emirevec <vecchioemiliano@gmail.com>
2024-02-05 16:38:33 -0300

    feat: add ButtonClose to Cart

--- src/views/cart/Cart.tsx
+++ src/views/cart/Cart.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from '../../components/Buttons/Button'
+import ButtonClose from '../../components/Buttons/ButtonClose'
 
 const Cart = (): JSX.Element => {
     return (
@@ -15,16 +16,7 @@ const Cart = (): JSX.Element => {
                                     <div className="flex items-start justify-between">
                                         <h2 className="text-lg font-medium text-gray-900" id="slide-over-title">Shopping cart</h2>
                                         <div className="ml-3 flex h-7 items-center">
-                                            <button id="cart_close" type="button"
-                                                className="relative -m-2 p-2 text-gray-400 hover:text-gray-500">
-                                                <span className="absolute -inset-0.5"></span>
-                                                <span className="sr-only">Close panel</span>
-                                                <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                                                    stroke="currentColor" aria-hidden="true">
-                                                    <path strokeLinecap="round" strokeLinejoin="round"
-                                                        d="M6 18L18 6M6 6l12 12" />
-                                                </svg>
-                                            </button>
+                                            <ButtonClose></ButtonClose>
                                         </div>
                                     </div>
                                     <div className="mt-8">
