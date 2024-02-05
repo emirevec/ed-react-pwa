c24013e71a8ea663a753aac38ab8cdeb90778cde
emirevec <vecchioemiliano@gmail.com>
2024-02-05 17:14:18 -0300

    feat: test prodct view in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
-import Navbar from './components/Navbar/Navbar'
-import Gallery from './components/Gallery/Gallery'
+import Product from './views/Product/Product'
 
 function App (): JSX.Element {
+    const data = {
+        id: 1,
+        title: 'Anteojos',
+        src: './src/images/products/glasses/Anteojos.jpg.webp',
+        price: 500
+    }
   return (
-    <div className="App">
-      <Navbar></Navbar>
-      <Gallery></Gallery>
-    </div>
-  )
+      <div className="App">
+        <Product data={data}></Product>
+      </div>
+    )
 }
 
 export default App
