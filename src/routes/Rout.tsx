72a9f0a688939a37df3fa3e3758aed36939461da
emirevec <vecchioemiliano@gmail.com>
2024-02-07 16:23:12 -0300

    feat: add home, product and card routes

--- src/routes/Rout.tsx
+++ src/routes/Rout.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../views/Home'
+import Product from '../views/Product/Product'
+import Cart from '../views/Cart/Cart'
 
 const Rout: React.FC = () => {
+    const data = {
+        id: 1,
+        title: 'Glasses',
+        src: 'public/src/images/products/glasses/Anteojos.jpg.webp',
+        price: 500
+    }
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' Component={ Home }/>
+                <Route path='/' element={ <Home/> }/>
+                <Route path='/product' element={ <Product data={data} /> }/>
+                <Route path='/cart' element={ <Cart/> }/>
             </Routes>
         </BrowserRouter>
     )
