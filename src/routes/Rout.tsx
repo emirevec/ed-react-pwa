f2c23b8364b21b29d5687bf6d8b2976394924b8a
emirevec <vecchioemiliano@gmail.com>
2024-02-12 11:10:32 -0300

    fix: correct path in Product Route

--- src/routes/Rout.tsx
+++ src/routes/Rout.tsx
@@ -8,14 +8,14 @@ const Rout: React.FC = () => {
     const data = {
         id: 1,
         title: 'Glasses',
-        src: 'public/src/images/products/glasses/Anteojos.jpg.webp',
+        src: './src/images/products/glasses/Anteojos.jpg.webp',
         price: 500
     }
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={ <Home/> }/>
-                <Route path='/product' element={ <Product data={data} /> }/>
+                <Route path='/product/:id' element={ <Product data={data} /> }/>
                 <Route path='/cart' element={ <Cart/> }/>
             </Routes>
         </BrowserRouter>
