c7122698847eac6ba86132732c9034c74083d8d5
emirevec <vecchioemiliano@gmail.com>
2024-02-15 18:07:17 -0300

    perf: extract CartItem from CartPage

--- src/views/CartPage.tsx
+++ src/views/CartPage.tsx
@@ -23,37 +23,7 @@ const Cart = (): JSX.Element => {
                                         <div className="flow-root">
                                             <ul role="list" className="-my-6 divide-y divide-gray-200">
                                                 Item
-                                                <li className="cart_li flex py-6">
-                                                    <div
-                                                        className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                                                        <img src=""
-                                                            alt="Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt."
-                                                            className="h-full w-full object-cover object-center" />
-                                                    </div>
-                                                    <div className="ml-4 flex flex-1 flex-col">
-                                                        <div>
-                                                            <div
-                                                                className="flex justify-between text-base font-medium text-gray-900">
-                                                                <h3>
-                                                                    <a href="#">Descripción</a>
-                                                                </h3>
-                                                                <p className="ml-4"> Precio </p>
-                                                            </div>
-                                                            <p className="mt-1 text-sm text-gray-500"> SKU:</p>
-                                                            <p className="cart_item_id mt-1 text-sm text-gray-500"> Id </p>
-                                                        </div>
-                                                        <div className="flex items-center gap-1 p-2">
-                                                            <p className=" items-center text-gray-500">Qty</p>
-                                                            <button className="cart_decrease rounded-md border border-gray-300 bg-white px-3 py-1  text-sm font-medium text-gray-700 hover:bg-gray-50">-</button>
-                                                            <p className=" items-center text-gray-500"> Count </p>
-                                                            <button className=" cart_increase rounded-md border border-gray-300 bg-white px-3 py-1 text-sm font-medium text-gray-700 hover:bg-gray-50">+</button>
-                                                        </div>
-                                                        <div className="flex">
-                                                            <button id="{{this.id}}" type="button"
-                                                                className="cart_remove_item font-medium text-strong hover:text-green-500">Remove</button>
-                                                        </div>
-                                                    </div>
-                                                </li>
+                                                
                                             </ul>
                                         </div>
                                     </div>
