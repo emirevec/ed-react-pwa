d705788f7e2d645dc0880311a878b3de180b1328
emirevec <vecchioemiliano@gmail.com>
2024-02-19 16:46:38 -0300

    perf: add EcommerceProvider context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
-import Rout from './routes/Rout'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import EcommerceProvider from './context/EcommerceProvider'
+import HomePage from './views/HomePage'
+import ProductPage from './views/ProductPage'
+import CartPage from './views/CartPage'
 
 function App (): JSX.Element {
     return (
         <div className="App">
-            <Rout></Rout>
+            <BrowserRouter>
+                <EcommerceProvider>
+                    <Routes>
+                        <Route path='/' element={ <HomePage/> }/>
+                        <Route path='/product/:id' element={ <ProductPage/> }/>
+                        <Route path='/cart' element={ <CartPage/> }/>
+                    </Routes>
+                </EcommerceProvider>
+            </BrowserRouter>
         </div>
     )
 }
