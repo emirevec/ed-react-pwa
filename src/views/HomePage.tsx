356627592fc2e3769b53d624a694154492f0da67
emirevec <vecchioemiliano@gmail.com>
2024-02-19 15:55:14 -0300

    perf: update Gallery source link

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import NavBar from '../components/Navbar/Navbar'
-import Gallery from '../components/Gallery/Gallery'
+import { Gallery } from '../components/Gallery'
 
 const HomePage = (): JSX.Element => {
     return (
