7f924c4cbf0927d91778f94c8c1ba15bebb9f4ca
emirevec <vecchioemiliano@gmail.com>
2024-02-19 16:00:32 -0300

    perf: update Navbar source link

--- src/views/HomePage.tsx
+++ src/views/HomePage.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import NavBar from '../components/Navbar/Navbar'
+import { Navbar } from '../components/Navbar'
 import { Gallery } from '../components/Gallery'
 
 const HomePage = (): JSX.Element => {
     return (
         <>
-        <NavBar></NavBar>
+        <Navbar></Navbar>
         <Gallery></Gallery>
         </>
     )
