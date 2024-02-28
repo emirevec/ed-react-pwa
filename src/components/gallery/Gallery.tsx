9eb31514a5113e4d1ff5343cb429a5803c30911d
emirevec <vecchioemiliano@gmail.com>
2024-02-28 10:25:33 -0300

    fix: correct the relation in context between provider/consumer

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { type Prod } from '../../types/types'
 import { GalleryCard } from './'
 import { useSearchParams } from 'react-router-dom'
@@ -8,23 +8,29 @@ type ProductList = Prod[]
 
 let Gallery: React.FC<any> = ({ dataSources }: any): JSX.Element => {
     const [products, setProducts] = useState<ProductList>([])
-    const fetchData = async (): Promise<void> => {
+    const [filteredProducts, setFilteredProducts] = useState<ProductList>([])
+    const [searchParams] = useSearchParams()
+    const filter: string | null = searchParams.get('q')
+    useEffect(() => {
+        const fetchData = async (): Promise<void> => {
         try {
-        const aux: ProductList = await dataSources.products().all()
-        console.log(aux)
+        const aux: ProductList = await dataSources.products.all()
         setProducts(aux)
+        setFilteredProducts(aux)
         } catch (error) {
             console.error('Error fetching products', error)
         }
     }
     void fetchData()
-    const [filteredProducts, setFilteredProducts] = useState<ProductList>([])
-    const [searchParams] = useSearchParams()
-    const filter: string | null = searchParams.get('q')
-    if (filter !== '' && filter != null) {
-        const filtered = products.filter(item => item.title.toLocaleLowerCase().includes(filter.toLocaleLowerCase()))
-        setFilteredProducts(filtered)
-    }
+    }, [])
+    useEffect(() => {
+        if (filter !== '' && filter != null) {
+            const filtered = products.filter(item => item.title.toLocaleLowerCase().includes(filter.toLocaleLowerCase()))
+            // console.log(filtered)
+            setFilteredProducts(filtered)
+            console.log(filtered)
+        }
+    }, [filter, products])
     return (
         <div className='flex justify-center'>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 m-2">
