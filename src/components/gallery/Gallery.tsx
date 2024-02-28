97b08dc7aa49c80e076c59c76bf10df96da16844
emirevec <vecchioemiliano@gmail.com>
2024-02-28 12:27:39 -0300

    perf: include ProductList type and export all types

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,16 +1,15 @@
 import { useEffect, useState } from 'react'
-import { type Prod } from '../../types/types'
+import { type ProductList } from '../../types/types'
 import { GalleryCard } from './'
 import { useSearchParams } from 'react-router-dom'
 import { withDataSources } from '../../context'
-
-type ProductList = Prod[]
 
 let Gallery: React.FC<any> = ({ dataSources }: any): JSX.Element => {
     const [products, setProducts] = useState<ProductList>([])
     const [filteredProducts, setFilteredProducts] = useState<ProductList>([])
     const [searchParams] = useSearchParams()
     const filter: string | null = searchParams.get('q')
+
     useEffect(() => {
         const fetchData = async (): Promise<void> => {
         try {
@@ -23,6 +22,7 @@ let Gallery: React.FC<any> = ({ dataSources }: any): JSX.Element => {
     }
     void fetchData()
     }, [])
+
     useEffect(() => {
         if (filter !== '' && filter != null) {
             const filtered = products.filter(item => item.title.toLocaleLowerCase().includes(filter.toLocaleLowerCase()))
@@ -31,6 +31,7 @@ let Gallery: React.FC<any> = ({ dataSources }: any): JSX.Element => {
             console.log(filtered)
         }
     }, [filter, products])
+
     return (
         <div className='flex justify-center'>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 m-2">
