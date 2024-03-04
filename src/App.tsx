69153d690ae2c0efa08536ee679c92ef0e7414c1
emirevec <vecchioemiliano@gmail.com>
2024-03-04 16:08:56 -0300

    feat: add store as Provider in app

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { EcommerceProvider } from './context'
 import DataSources from './data'
 import { HomePage, ProductPage, CartPage } from './views'
+import { Provider } from 'react-redux'
+import store from './redux'
 
 function App (): JSX.Element {
     return (
         <div className="App">
-            <BrowserRouter>
-                <EcommerceProvider value={DataSources}>
-                    <Routes>
-                        <Route path='/' element={ <HomePage/> }/>
-                        <Route path='/product/:id' element={ <ProductPage/> }/>
-                        <Route path='/cart' element={ <CartPage/> }/>
-                    </Routes>
-                </EcommerceProvider>
-            </BrowserRouter>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <EcommerceProvider value={DataSources}>
+                        <Routes>
+                            <Route path='/' element={ <HomePage/> }/>
+                            <Route path='/product/:id' element={ <ProductPage/> }/>
+                            <Route path='/cart' element={ <CartPage/> }/>
+                        </Routes>
+                    </EcommerceProvider>
+                </BrowserRouter>
+            </Provider>
         </div>
     )
 }
