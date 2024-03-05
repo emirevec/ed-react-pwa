e8f36c4f27ad984cba1e0cf38707963c5840c742
emirevec <vecchioemiliano@gmail.com>
2024-03-05 11:02:18 -0300

    fix: specify types into actions.tsx

--- src/redux/cart/actions.tsx
+++ src/redux/cart/actions.tsx
@@ -1,3 +1,6 @@
+import { type Action } from 'redux'
+import type { Prod } from '../../types/types'
+
 import {
     CART_ADDITEM,
     CART_REMOVEITEM,
@@ -5,22 +8,33 @@ import {
     CART_DECREASECOUNT
 } from './action_types'
 
-export const cartAddItem = (id, sizes, colors) => ({
+interface Payload {
+    id: Prod['id']
+    sizes: Prod['sizes']
+    colors: Prod['colors']
+}
+
+interface CartItemAction extends Action {
+    type: string
+    payload: Payload
+}
+
+export const cartAddItem = (id: Prod['id'], sizes: Prod['sizes'], colors: Prod['colors']): CartItemAction => ({
     type: CART_ADDITEM,
     payload: { id, sizes, colors }
 })
 
-export const cartRemoveItem = (id, sizes, colors) => ({
+export const cartRemoveItem = (id: Prod['id'], sizes: Prod['sizes'], colors: Prod['colors']): CartItemAction => ({
     type: CART_REMOVEITEM,
     payload: { id, sizes, colors }
 })
 
-export const cartIncreaseCount = (id, sizes, colors) => ({
+export const cartIncreaseCount = (id: Prod['id'], sizes: Prod['sizes'], colors: Prod['colors']): CartItemAction => ({
     type: CART_INCREASECOUNT,
     payload: { id, sizes, colors }
 })
 
-export const cartDecreaseCount = (id, sizes, colors) => ({
+export const cartDecreaseCount = (id: Prod['id'], sizes: Prod['sizes'], colors: Prod['colors']): CartItemAction => ({
     type: CART_DECREASECOUNT,
     payload: { id, sizes, colors }
 })
